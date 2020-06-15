4b631f2cde51c0a4e19363e452999d6abb5d6aee
ducklett <ivy16203@gmail.com>
2020-06-15 15:39:26 +0200

    add a benchmark macro

--- src/main.c
+++ src/main.c
@@ -4,14 +4,17 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "syntax/syntaxTree.c"
 #include "diagnostics.c"
 #include "syntax/lexer.c"
 #include "syntax/parser.c"
 
-int main()
-{
+#define benchmark_start() clock_t t = clock(); int acc = 0;
+#define benchmark_end(name) t = clock() - t; printf("%s: %.0fms\n", name, ((double)t) / CLOCKS_PER_SEC * 1000);
+
+int main() {
 	char text[] = "10 + 10 * (20 + 10)";
 
 	lexer l = { .text = text, .text_length = sizeof(text), .index = 0, };
@@ -21,7 +24,10 @@ int main()
 	diagnosticContainer diagnostics = {0};
 
 	printf("Input: %s\n", text);
+
+	benchmark_start();
 	parser_parse(&p, &diagnostics);
+	benchmark_end("Parsing");
 
 	if (diagnostics.index==0)
 		print_syntaxtree(text, &p.root, 0, true);
