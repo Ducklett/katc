cfe57c999e02be65256963ae48c7440c3ee805ae
ducklett <ivy16203@gmail.com>
2020-07-13 20:15:39 +0200

    improve readability of emitted c code

--- src/emit/Cemitter.c
+++ src/emit/Cemitter.c
@@ -18,6 +18,38 @@ static inline void emit_c_variableAssignment(astNode *n, ast *tree);
 static inline void emit_c_variableReference(astNode *n, ast *tree);
 char* escape_string_c(char *str);
 
+int c_indent=4;
+enum astKind kindStack[128];
+u8 kindIndex = 0;
+enum astKind parentKind;
+
+enum astKind needs_indent(enum astKind k) {
+	return (
+		k == blockStatementKind ||
+		k == caseStatementKind  );
+}
+
+enum astKind needs_semicolon(enum astKind k) {
+	return !(
+		k == blockStatementKind ||
+		k == forLoopKind        ||
+		k == whileLoopKind      ||
+		k == ifStatementKind    ||
+		k == caseStatementKind  );
+}
+inline static void kindStack_push(enum astKind k) {
+	kindStack[++kindIndex] = k;
+	parentKind = kindStack[kindIndex-1];
+
+	if (needs_indent(k)) c_indent+=4;
+}
+
+inline static void kindStack_pop() {
+	if (needs_indent(kindStack[kindIndex])) c_indent-=4;
+	--kindIndex;
+	parentKind = kindStack[kindIndex];
+}
+
 static const char *cTypeText[] = {
 	"errorType",
 	"unresolved",
@@ -91,29 +123,33 @@ void emit_c_from_ast(ast *tree, const char* outputName, bool run, bool emitSourc
 }
 
 void emit_c_node(astNode *n, ast *tree) {
+
+	kindStack_push(n->kind);
 	switch(n->kind) {
-	case blockStatementKind: return emit_c_blockStatement(n, tree);
-	case ifStatementKind: return emit_c_ifStatement(n, tree);
-	case caseStatementKind: return emit_c_caseStatement(n, tree);
-	case switchStatementKind: return emit_c_switchStatement(n, tree);
-	case whileLoopKind: return emit_c_whileLoop(n, tree);
-	case forLoopKind: return emit_c_forLoop(n, tree);
+	case fileStatementKind:
+	case blockStatementKind: emit_c_blockStatement(n, tree); break;
+	case ifStatementKind: emit_c_ifStatement(n, tree); break;
+	case caseStatementKind: emit_c_caseStatement(n, tree); break;
+	case switchStatementKind: emit_c_switchStatement(n, tree); break;
+	case whileLoopKind: emit_c_whileLoop(n, tree); break;
+	case forLoopKind: emit_c_forLoop(n, tree); break;
 
 	case breakKind: fprintf(fp, "break;\n"); break;
 	case continueKind: fprintf(fp, "continue;\n"); break;
 
-	case literalKind: return emit_c_literal(n, tree);
-	case binaryExpressionKind: return emit_c_binaryExpression(n, tree);
-	case unaryExpressionKind: return emit_c_unaryExpression(n, tree);
-	case callExpressionKind: return emit_c_callExpression(n, tree);
-	case castExpressionKind: return emit_c_castExpression(n, tree);
-	case variableDeclarationKind: return emit_c_variableDeclaration(n, tree);
-	case variableAssignmentKind: return emit_c_variableAssignment(n, tree);
-	case variableReferenceKind: return emit_c_variableReference(n, tree);
+	case literalKind: emit_c_literal(n, tree); break;
+	case binaryExpressionKind: emit_c_binaryExpression(n, tree); break;
+	case unaryExpressionKind: emit_c_unaryExpression(n, tree); break;
+	case callExpressionKind: emit_c_callExpression(n, tree); break;
+	case castExpressionKind: emit_c_castExpression(n, tree); break;
+	case variableDeclarationKind: emit_c_variableDeclaration(n, tree); break;
+	case variableAssignmentKind: emit_c_variableAssignment(n, tree); break;
+	case variableReferenceKind: emit_c_variableReference(n, tree); break;
 	default:
 		fprintf(stderr, "%sUnhandled node of type %s in c emitter%s", TERMRED, astKindText[n->kind], TERMRESET);
 		exit(1);
 	}
+	kindStack_pop();
 }
 
 void emit_c_file(astNode *n, ast *tree) {
@@ -125,25 +161,25 @@ void emit_c_file(astNode *n, ast *tree) {
 		fprintf(fp,"\n");
 	}
 	fprintf(fp,"void main() ");
-	emit_c_blockStatement(n,tree);
+	emit_c_node(n,tree);
 }
 
 static inline void emit_c_function(astNode *n, ast *tree) {
-
 	astSymbol vn = *(astSymbol*)n->data;
-	fprintf(fp,"%s %s() ", cTypeText[vn.type], vn.name);
-	emit_c_blockStatement(&vn.functionData->body,tree);
+	fprintf(fp,"%*s%s %s() ", c_indent, "", cTypeText[vn.type], vn.name);
+	emit_c_node(&vn.functionData->body,tree);
 }
 
 static inline void emit_c_blockStatement(astNode *n, ast *tree) {
-	fprintf(fp,"{\n");
+	fprintf(fp,"%*s{\n", c_indent-4, "");
 	blockStatementAst bn = *(blockStatementAst*)n->data;
 	for (int i= 0; i < bn.statementsCount; i++) {
 		if (bn.statements[i].kind == functionDeclarationKind) continue;
+		fprintf(fp,"%*s", c_indent, "");
 		emit_c_node(bn.statements + i, tree);
-		fprintf(fp,";\n");
+		if(needs_semicolon(bn.statements[i].kind)) fprintf(fp,";\n");
 	}
-	fprintf(fp,"}\n");
+	fprintf(fp,"%*s}\n", c_indent-4, "");
 }
 
 static inline void emit_c_ifStatement(astNode *n, ast *tree) {
@@ -151,11 +187,16 @@ static inline void emit_c_ifStatement(astNode *n, ast *tree) {
 	fprintf(fp,"if (");
 	emit_c_node(&in.condition, tree);
 	fprintf(fp,")\n");
+
+	fprintf(fp,"%*s", c_indent, "");
 	emit_c_node(&in.thenStatement, tree);
-	if (in.thenStatement.kind != blockStatementKind) fprintf(fp,";\n");
+	if (needs_semicolon(in.thenStatement.kind)) fprintf(fp,";\n");
 	if (in.elseStatement.kind != 0) {
-		fprintf(fp," else \n");
-		emit_c_node(&in.elseStatement, tree);
+		fprintf(fp,"%*s", c_indent, "");
+		fprintf(fp,"else \n");
+		fprintf(fp,"%*s", c_indent, "");
+		emit_c_node(&in.elseStatement.kind, tree);
+		if (needs_semicolon(in.elseStatement.kind)) fprintf(fp,";\n");
 	}
 }
 
@@ -164,13 +205,13 @@ static inline void emit_c_caseStatement(astNode *n, ast *tree) {
 	for (int i=0;i<cn.branchCount;i++) {
 		caseBranchAst cb = *(caseBranchAst*)((cn.branches + i)->data);
 		if (cb.condition.kind ==0) {
-			fprintf(fp,"else ");
+			fprintf(fp,"%*selse ", c_indent, "");
 		} else if (i==0) {
 			fprintf(fp,"if (");
 			emit_c_node(&cb.condition, tree);
 			fprintf(fp,") ");
 		} else {
-			fprintf(fp,"else if (");
+			fprintf(fp,"%*selse if (", c_indent, "");
 			emit_c_node(&cb.condition, tree);
 			fprintf(fp,") ");
 		}
@@ -187,17 +228,17 @@ static inline void emit_c_switchStatement(astNode *n, ast *tree) {
 	for (int i=0;i<cn.branchCount;i++) {
 		switchBranchAst cb = *(switchBranchAst*)((cn.branches + i)->data);
 		if (cb.condition.kind == 0) {
-			fprintf(fp,"default: ");
+			fprintf(fp,"%*sdefault: ",c_indent,"");
 		} else {
 			if (cb.condition.kind == rangeExpressionKind) {
 				rangeExpressionAst rn = *(rangeExpressionAst*)cb.condition.data;
 				for (int i=rn.fromInt; i <= rn.toInt; i++) {
-					if (cb.condition.type == charType) fprintf(fp,"case '%c': ", i);
-					else fprintf(fp,"case %d: ", i);
+					if (cb.condition.type == charType) fprintf(fp,"%*scase '%c': ",c_indent,"", i);
+					else fprintf(fp,"%*scase %d: ", c_indent, "", i);
 				}
 				fprintf(fp,"\n");
 			} else {
-				fprintf(fp,"case ");
+				fprintf(fp,"%*scase ", c_indent, "");
 				emit_c_node(&cb.condition, tree);
 				fprintf(fp,": ");
 			}
@@ -212,7 +253,7 @@ static inline void emit_c_whileLoop(astNode *n, ast *tree) {
 	whileLoopAst wn = *(whileLoopAst*)n->data;
 	fprintf(fp,"while (");
 	emit_c_node(&wn.condition, tree);
-	fprintf(fp,") ");
+	fprintf(fp,")\n");
 	emit_c_node(&wn.block, tree);
 }
 
@@ -231,17 +272,25 @@ static inline void emit_c_forLoop(astNode *n, ast *tree) {
 		fprintf(fp,"%s %s %d; ", fn.value->name, compOp, rn.toInt);
 		// x++
 		fprintf(fp,"%s%s", fn.value->name, incOp);
-		fprintf(fp,") ");
+		fprintf(fp,")\n");
+
+		if (fn.block.kind != blockStatementKind) fprintf(fp,"%*s", c_indent+4, "");
 		emit_c_node(&fn.block, tree);
+		if (needs_semicolon(fn.block.kind))fprintf(fp,";\n");
 	} else {
 		fprintf(fp,"for (int %s = 0; %s <= %d; %s++) {\n", fn.index->name, fn.index->name, abs(rn.fromInt - rn.toInt), fn.index->name);
+		kindStack_push(blockStatementKind);
 
+		fprintf(fp,"%*s", c_indent, "");
 		if (rn.toInt > rn.fromInt) fprintf(fp,"%s %s = i + %d;\n", cTypeText[fn.value->type], fn.value->name, rn.fromInt);
 		else fprintf(fp,"%s %s = %d - i;\n",cTypeText[fn.value->type], fn.value->name, rn.fromInt);
 
+		if (fn.block.kind != blockStatementKind) fprintf(fp,"%*s", c_indent, "");
 		emit_c_node(&fn.block, tree);
-		fprintf(fp,";\n");
-		fprintf(fp,"} ");
+		if (needs_semicolon(fn.block.kind)) fprintf(fp,";\n");
+		kindStack_pop();
+		fprintf(fp,"%*s", c_indent, "");
+		fprintf(fp,"}\n");
 	}
 }
 
@@ -264,16 +313,18 @@ static inline void emit_c_literal(astNode *n, ast *tree) {
 
 static inline void emit_c_binaryExpression(astNode *n, ast *tree) {
 	binaryExpressionAst bn = *(binaryExpressionAst*)n->data;
-	fprintf(fp,"(");
+	bool needsParen = parentKind == binaryExpressionKind || parentKind == unaryExpressionKind;
+	if (needsParen) fprintf(fp,"(");
 	emit_c_node(&bn.left, tree);
 	fprintf(fp," %s ", cBinaryText[bn.operator]);
 	emit_c_node(&bn.right, tree);
-	fprintf(fp,")");
+	if (needsParen) fprintf(fp,")");
 }
 
 static inline void emit_c_unaryExpression(astNode *n, ast *tree) {
 	unaryExpressionAst un = *(unaryExpressionAst*)n->data;
-	fprintf(fp,"(");
+	bool needsParen = parentKind == binaryExpressionKind || parentKind == unaryExpressionKind;
+	if (needsParen) fprintf(fp,"(");
 	if (un.operator == postIncrementOp || un.operator == postDecrementOp) {
 		emit_c_node(&un.operand, tree);
 		fprintf(fp,"%s", cUnaryText[un.operator]);
@@ -281,11 +332,12 @@ static inline void emit_c_unaryExpression(astNode *n, ast *tree) {
 		fprintf(fp,"%s", cUnaryText[un.operator]);
 		emit_c_node(&un.operand, tree);
 	}
-	fprintf(fp,")");
+	if (needsParen) fprintf(fp,")");
 }
 
 static inline void emit_c_callExpression(astNode *n, ast *tree) {
 	callExpressionAst cn = *(callExpressionAst*)n->data;
+
 	fprintf(fp,"printf(");
 	for (int i= 0; i < cn.argumentCount; i++) {
 		emit_c_node(cn.arguments + i, tree);
